1d16c7a9f952fb10917b93d6a0cb09eedb1d8df0
alkalim <33055344+alkalim@users.noreply.github.com>
2022-08-17 00:59:30 +0300

    added functions for commands

--- minictinit.sh
+++ minictinit.sh
@@ -13,6 +13,8 @@ service_file=/etc/systemd/system/minictinit.service
 run_dir=/usr/bin
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 target_exe_file=$run_dir/$BASH_SOURCE
+zabbix_conf=/etc/zabbix/zabbix_agentd.conf
+zabbix_conf_dir=/etc/zabbix/zabbix_agentd.conf.d
 
 function log()
 {
@@ -29,28 +31,50 @@ function die()
 function usage()
 {
     cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}") install | start | uninstall
+Usage: $(basename "${BASH_SOURCE[0]}") install | start [<functions>]| uninstall
+    Container initialization script
 Commands:
     install     - install systemd service
     start       - start service (should be run by systemd)
     uninstall   - remove systemd service
+Functions:
+    ssh         - recreate ssh host keys
+    zabbix      - add zabbix autoregistration settings
 EOF
     exit
 }
 
 function start()
 {
+    IFS=',' read -ra funcs <<< "$funcs"
+
+    for func in "${funcs[@]}"; do
+        echo $func
+    done
+
+    case $func in
+        ssh)
+            log "recreate ssh host keys"
+            if [ ! -f "$ssh_key" ]; then 
+                log "$ssh_key not found, running dpkg-reconfigure"
+                dpkg-reconfigure openssh-server
+            fi
+            ;;
+        zabbix)
+            log "add zabbix autoregistration settings"
+            sed -i "s/^Hostname=\(.*\)/# Hostname=\1/" $zabbix_conf
+            ;;
+        *)
+            die "unknown function: $func"
+            ;;
+    esac
+
     if [ -f "$flag_file" ]; then
-        log "Found $flag_file, exiting"
+        log "found $flag_file, exiting"
         exit
     fi
 
-    if [ ! -f "$ssh_key" ]; then 
-        log "$ssh_key not found, running dpkg-reconfigure"
-        dpkg-reconfigure openssh-server
-    fi
-
-    log "Creating $flag_file"
+    log "creating $flag_file"
     >$flag_file
 
     exit
@@ -58,13 +82,18 @@ function start()
 
 function install()
 {
+    funcs=$1
+    if [ -z "$funcs" ]; then
+        funcs="ssh,zabbix"
+    fi
+
     # Create systemd unit file
     cat >$service_file <<EOF
 [Unit]
 Description=Mini Container Init Script
 
 [Service]
-ExecStart=$target_exe_file start
+ExecStart=$target_exe_file start $funcs
 
 [Install]
 WantedBy=multi-user.target
@@ -117,11 +146,11 @@ while [ $# -gt 0 ]; do
     case $1 in
         install)
             shift
-            install
+            install $1
             ;;
         start)
             shift
-            start
+            start $1
             ;;
         uninstall)
             shift
