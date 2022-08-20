4fa76905b63236881cedcbdc79b2b2c8025b98b6
alkalim <33055344+alkalim@users.noreply.github.com>
2022-08-20 18:56:03 +0000

    added more improvements, clearer usage, license

--- minictinit.sh
+++ minictinit.sh
@@ -16,35 +16,45 @@ target_exe_file=$run_dir/$BASH_SOURCE
 zabbix_conf=/etc/zabbix/zabbix_agentd.conf
 zabbix_conf_dir=/etc/zabbix/zabbix_agentd.conf.d
 
-function log()
+function log
 {
     echo $(date "+%Y/%m/%d %H:%M:%S ") $@ >> $log_file
 }
 
-function die()
+function die
 {
     echo -n "fatal: "
     echo $@
     exit -1
 }
 
-function usage()
+function usage
 {
     cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}") install <functions> | start [<functions>] | uninstall
-    Container initialization script
+Usage: $(basename "${BASH_SOURCE[0]}") <command>
+    Initialization script for LXC-based Debian/Ubuntu containers
 Commands:
-    install     - install systemd service
-    start       - start service (should be run by systemd, not manually)
-    uninstall   - remove systemd service
+    install <functions>  - install systemd service
+    start <functions>    - start service (should be run by systemd)
+    uninstall            - remove systemd service
 Functions:
-    ssh                      - recreate ssh host keys
-    zabbix(<server_ip>)      - configure Zabbix agent (add autoregistration, server ip, etc)
+    ssh                        - configure ssh (recreate host keys,
+                                 permit root, enable service)
+    zabbix(<server_ip>)        - configure Zabbix agent (add autoregistration,
+                                 set server ip, change log limit)
+    editor(<path_to_editor>)   - set default editor in /etc/bash.bashrc file
+Example:
+./minictinit.sh install ssh,zabbix(zbx.example.com),editor(/usr/bin/vi)
 EOF
     exit
 }
 
-function start()
+function parse_func_arg
+{
+    echo $(echo $@|sed -e '/(/!d' -e '/)/!d' -e 's/^.*(\(.*\))/\1/')
+}
+
+function start
 {
     IFS=',' read -ra funcs <<< "$1"
 
@@ -61,11 +71,21 @@ function start()
                 if [ -f "$ssh_config" ]; then
                     sed -i "s/#PermitRootLogin /PermitRootLogin /" $ssh_config
                 fi
+                systemctl stop ssh
+                systemctl enable --now ssh
+                ;;
+            editor*)
+                editor=$(parse_func_arg $func)
+                cat >>/etc/bash.bashrc <<EOF
+# added by minictinit.sh
+export EDITOR=$editor
+export VISUAL=\$EDITOR
+EOF
                 ;;
             zabbix*)
                 log "configuring Zabbix agent"
                 if [ -f "$zabbix_conf" ]; then
-                    server_ip=$(echo $func|sed "s/^zabbix(\(.*\))/\1/")
+                    server_ip=$(parse_func_arg $func)
                     sed -i "s/^Hostname=\(.*\)/# Hostname=\1/" $zabbix_conf
                     sed -i "s/^LogFileSize=0/LogFileSize=5M/" $zabbix_conf
                     sed -i "s/^Server=127.0.0.1/Server=$server_ip/" $zabbix_conf
@@ -73,6 +93,7 @@ function start()
                     sed -i "s/# HostnameItem=system.hostname/HostnameItem=minictinit.hostname/" $zabbix_conf
                     sed -i "s/# HostMetadataItem=/HostMetadataItem=minictinit.metadata/" $zabbix_conf
                     cat > /etc/zabbix/zabbix_agentd.d/minictinit.conf <<EOF
+# added by minictinit.sh
 UserParameter=minictinit.hostname,echo pm-$(hostname)
 UserParameter=minictinit.metadata,echo Linux-pm-$(cat /etc/machine-id)
 EOF
@@ -97,12 +118,14 @@ EOF
     exit
 }
 
-function install()
+function install
 {
     funcs=$1
     if [ -z "$funcs" ]; then
-        funcs="ssh,zabbix(127.0.0.1)"
+        funcs="ssh,zabbix(127.0.0.1),editor(/usr/bin/vi)"
     fi
+
+    log "installing systemd service with functions $funcs"
 
     # Create systemd unit file
     cat >$service_file <<EOF
@@ -142,7 +165,7 @@ function uninstall()
     exit
 }
 
-function check_distro()
+function check_distro
 {
     if [ $(uname) != "Linux" ]; then
         die "not running under Linux"
